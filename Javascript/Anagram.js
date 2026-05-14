990f2fe6e7d71e3b394e8a73e9d6621eee3e36f6
Mohamed Rizwan <mmohamedrijwan@gmail.com>
2026-05-14 20:06:49 +0530

    Add IsAnagram function and improve SwapVariable examples

--- Javascript/Anagram.js
+++ Javascript/Anagram.js
@@ -7,3 +7,87 @@ const sorted1 = str1.split("").sort().join("");
 const sorted2 = str1.split("").sort().join("");
 
 console.log(sorted1 === sorted2);
+
+// Solution 2
+
+function IsAnagram(input1, input2)
+{
+    if (input1.length !== input2.length) { return false; }
+
+    const map = {};
+
+    for (const ch of input1)
+    {
+        if (map[ch] != null)
+        {
+            map[ch]++;
+        }
+        else
+        {
+            map[ch] = 1;
+        }
+    }
+
+    for (const ch of input2)
+    {
+        if (map[ch] == null)
+        {
+            return false;
+        }
+
+        map[ch]--;
+
+        if (map[ch] < 0)
+        {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+console.log(IsAnagram(str1, str2));
+
+/**
+ * Dictionary Approach
+ * 
+ * Instead of sorting:
+ * 
+ * 1. Count characters in first string
+ * 2. Reduce counts using second string
+ * 
+ * Example:
+ * listen
+ * silent
+ * 
+ * Dictionary after first string:
+ * l → 1
+ * i → 1
+ * s → 1
+ * t → 1
+ * e → 1
+ * n → 1 
+ * 
+ * After second string:
+ * l → 0
+ * i → 0
+ * s → 0
+ * t → 0
+ * e → 0
+ * n → 0
+ * 
+ * All become 0 → anagram.
+ * 
+ * Why Faster?
+ * 
+ * Because dictionary operations are almost constant time:
+ * O(1)
+ * 
+ * So traversing all characters once gives:
+ * O(n)
+ * 
+ * Total complexity:
+ * 
+ * Separate loops are added: O(n) + O(n)
+ * which simplies to: O(n)
+ */
--- Javascript/SwapVariable.js
+++ Javascript/SwapVariable.js
@@ -2,10 +2,24 @@
  * Swap variable without using temp variable
  */
 
+// Solution 1
+
 let a = 10;
 let b = 20;
 
 [a, b] = [b, a];
+
+console.log(a);
+console.log(b);
+
+// Solution 2
+
+let a1 = 10;
+let b1 = 20
+
+a1 = a1 + b1;
+b1 = a1 - b1;
+a1 = a1 - b1;
 
 console.log(a);
 console.log(b);
